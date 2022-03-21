46fb07058b0d6d93bf1100c4de32aab0be509178
Thomas Bender <thomasmatthewbender@gmail.com>
2022-03-21 09:08:06 -0400

    commit tree 055261f72d8d316c8b037886ba89b52ca4930a5e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+const upperCaseCustomerName = () => customerName.toUpperCase
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+
+const overwriteBestCustomer = () => bestCustomer.shift;
+
+const leastFavoriteCustomer = 'some initial value'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'blah'
+    return leastFavoriteCustomer
+}
